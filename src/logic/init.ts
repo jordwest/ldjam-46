8b9f6117c4afdf908e6a1594dfa0c7109a97fc56
Jordan West <jordwest@gmail.com>
2020-04-20 17:03:17 +1000

    init camps and stones

--- src/logic/init.ts
+++ src/logic/init.ts
@@ -9,6 +9,17 @@ import { Audio } from "../audio/audio";
 import { createCamp } from "./entities/camp";
 import { createCursor } from "./entities/cursor";
 import { createStone } from "./entities/stone";
+import { Vec2 } from "~base/vec2";
+import { expect } from "~base/expect";
+import { Angle } from "~base/angle";
+
+const NUM_CAMPS = 80;
+const NUM_STONES = 250;
+
+const PLAY_AREA_SIZE = {
+  x: 200,
+  y: 200,
+};
 
 export function init(renderState: RenderState): GameState {
   const randomSource = new Uint8Array(1024);
@@ -37,7 +48,7 @@ export function init(renderState: RenderState): GameState {
     hand: new Map(),
   };
 
-  const startLocation = { x: 30, y: 30 };
+  const startLocation = Vec2.divScalar(PLAY_AREA_SIZE, 2);
   createPlayer(player, startLocation, components);
   const cursor = createCursor(entities, components);
   const audioState = Audio.init();
@@ -52,6 +63,7 @@ export function init(renderState: RenderState): GameState {
     components,
     stats: {
       fearBar: 0.9,
+      dead: false,
     },
     inputs: {
       moveUp: false,
@@ -69,9 +81,39 @@ export function init(renderState: RenderState): GameState {
     },
   };
 
-  createCamp(state, { x: 40, y: 29 });
-
-  createStone(state, { x: 35, y: 30 });
+  generateEntities(state, PLAY_AREA_SIZE);
 
   return state;
+}
+
+function generateEntities(state: GameState, area: Vec2) {
+  // Create one camp near the player
+  const distance = 8;
+  const angle = Math.random() * (Math.PI * 2);
+  const playerPos = expect(
+    state.components.position.get(state.entities.player)
+  );
+  const initialCampPos = Vec2.add(
+    playerPos,
+    Vec2.multScalar(Angle.toVec(angle), distance)
+  );
+  createCamp(state, initialCampPos);
+
+  const getRandomLocation = () => ({
+    x: Math.random() * area.x,
+    y: Math.random() * area.y,
+  });
+  for (let i = 0; i < NUM_CAMPS; i++) {
+    let pos = getRandomLocation();
+    const distanceToPlayer = Vec2.distance(pos, playerPos);
+    // Don't create more camps close to the player
+    if (distanceToPlayer > 10) {
+      createCamp(state, pos);
+    }
+  }
+
+  for (let i = 0; i < NUM_STONES; i++) {
+    let pos = getRandomLocation();
+    createStone(state, pos);
+  }
 }
